87d80611ead1057cc4eb997a5b1329cd16a95ad3
Saaim <saaim.arfin@gmail.com>
2025-07-22 12:37:24 +0530

    main file hai baba

---
+++ main.tf
@@ -0,0 +1,24 @@
+resource "azurerm_resource_group" "rg1" {
+  name     = var.rg1
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "rg2" {
+  name     = "Oppo"
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "rg3" {
+  name     = var.rg3
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "rg4" {
+  name     = var.rg4
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "rg5" {
+  name     = var.rg5
+  location = "West Europe"
+}
